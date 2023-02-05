6447a151e87a1b16bc84931f63face0fae7cef50
Armada <yaskan@gmail.com>
2023-02-05 22:48:57 +0300

    Armada-Initial commit

---
+++ src/Day01_ComputerScience/ComputerScience.java
@@ -0,0 +1,7 @@
+package Day01_ComputerScience;
+
+public class ComputerScience {
+    public static void main(String[] args) {
+        System.out.println("Computer Science is the first day.");
+    }
+}
