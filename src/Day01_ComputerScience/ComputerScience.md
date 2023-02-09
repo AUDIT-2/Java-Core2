e5bb7af228a8d61be36d5157680ac2c65a8c5390
Armada <yaskan@gmail.com>
2023-02-09 22:52:26 +0300

    Armada-Initial commit

---
+++ src/Day01_ComputerScience/ComputerScience.md
@@ -0,0 +1,13 @@
+ <a href="https://bootflow.academy/" target="_blank" rel="noreferrer"> <img src="logo-grey-blue.png" alt="BootFlow Academy"/> </a>
+
+# [BootFlow Academy](https://bootflow.academy/)
+
+# Turkish Batch-2 (February 2023)
+# Software Development Engineer in Test (SDET)
+## Core Java programming for SDET
+
+### Course cover
+
+* Day 1- Introduction Computer Science | Algorithm
+* Day 2- Introduction to Java |JVM, JRE, JDK
+* [Day 02- MyFirstProgram | Print & Println | Comments](https://github.com/BootFlowAcademy/Java-Core2/tree/master/src/Day02_MyFirstProgram)
--- src/Day02_MyFirstProgram/MyFirstProgram.java
+++ src/Day02_MyFirstProgram/MyFirstProgram.java
@@ -2,9 +2,14 @@ package Day02_MyFirstProgram;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        // Java da ilk programım.---> Comment(Yorum)
+        // Ekrana Hello World yazan bir program.
         System.out.println("Hello World.");
-        System.out.println("Hello World.");
-        System.out.println("Hello World.");
-        System.out.println("Hello World.");
+        System.out.println(); //Ekrana boş bir satır yazan Java komut.
+        System.out.println("          Ben Java öğreniyorum.");
+        System.out.println(1234);//Sayısal ifadeler için çift tırnak kullanmaya gerek yok
+        System.out.println("Bu benim ilk programım.");
+
+        System.out.println(5+34); // 5 ve 34 sayılarının toplamını ekrana(console) yazdıran komut satırı
     }
 }
