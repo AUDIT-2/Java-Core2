e5266ae7bf0b279de89a4d4feaa86af73ccd7e96
Armada <yaskan@gmail.com>
2023-02-10 00:32:23 +0300

    Armada-Some Updates

--- src/Day02_MyFirstProgram/MyFirstProgram.java
+++ src/Day02_MyFirstProgram/MyFirstProgram.java
@@ -7,7 +7,7 @@ public class MyFirstProgram {
         System.out.println("Hello World.");
         System.out.println(); //Ekrana boş bir satır yazan Java komutu.
         System.out.println("          Ben Java öğreniyorum.");
-        System.out.println(1234);//Sayısal ifadeler için çift tırnak kullanmaya gerek yok
+        System.out.println(1234);//Sayısal ifadeler için çift tırnak kullanmaya gerek yok.
         System.out.println("Bu benim ilk programım.");
 
         System.out.println(5+34); // 5 ve 34 sayılarının toplamını ekrana(console) yazdıran komut satırı
