38e86ab61d269e24ab558aa9d74b48341d3087dd
Armada <yaskan@gmail.com>
2023-02-11 23:24:12 +0300

    Armada-Some Updates

---
+++ src/Day03_Variables1/HizUzaklikHesaplama.java
@@ -0,0 +1,22 @@
+package Day03_Variables1;
+
+public class HizUzaklikHesaplama {
+    public static void main(String[] args) {
+        System.out.println("Hız, uzaklık ve süre hesaplama programı.");
+        //Distance = Speed * Time
+        int distance; //Hesaplanacak mesafe değişkeni
+        int speed;
+        int time;
+
+        distance = 100 * 5;
+        System.out.println("Katedilen mesafe : " + distance + " Km");
+
+        speed = 150;
+        time = 7;
+        distance = speed * time;
+        System.out.println("Hız = " + speed + " (Km/saat)");
+        System.out.println("Süre = " + time + " (Saat)");
+        System.out.println("Katedilen mesafe : " + distance + " Km");
+
+    }
+}
