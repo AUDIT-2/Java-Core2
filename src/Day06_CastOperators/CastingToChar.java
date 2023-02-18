92134b97305f5ad6246f07c0a1589a4cd04156c0
Armada <yaskan@gmail.com>
2023-02-19 00:54:00 +0300

    Armada-Some Updates

---
+++ src/Day06_CastOperators/CastingToChar.java
@@ -0,0 +1,25 @@
+package Day06_CastOperators;
+
+public class CastingToChar {
+    public static void main(String[] args) {
+        char harf1, harf2, harf3;
+        int num = 65;
+        harf1 = (char) num; // Explicit Casting- int --> char narrowing
+        System.out.println("char deger = " + harf1); // ASCII code tablosundaki 65 sayısının char tipine dönüştürülmesi sonucu 'A' harfini yazdırır.
+
+        int num2 = harf1; // Implicit Casting --> char to int automatic casting-widening
+        System.out.println("int deger = " + num2);
+
+        harf2 = 'B';
+        harf3 = 67;
+
+        System.out.println("Harf1, harf2 , harf 3 : " + harf1 + " - " +  harf2 + " - " + harf3);
+
+        char harf4 = 32755;
+        System.out.println("harf4 = " + harf4);
+
+        //Boolean can not be casted to any primitive data types
+        boolean bl = true;
+        //int i = (int) bl; //Hata verir
+    }
+}
---
+++ src/Day06_CastOperators/TypePromotion2.java
@@ -0,0 +1,41 @@
+package Day06_CastOperators;
+
+public class TypePromotion2 {
+
+    public static void main(String[] args) {
+
+        //Bir işlemde işleme tabi tutulan(operand) long, float veya double ise, tüm işlem sırasıyla long, float veya double olarak yükseltilir.
+        //Promote edilir.
+
+        
+        byte myByte = 42;
+        char myChar = 'a';
+        short myShort = 1024;
+        int myInt = 50000;
+        long myLong = 31514162837468746L;
+        float myFloat = 5.4f;
+        double myDouble = 0.25353;
+
+        //Expression--işlem
+        //(myFloat * myByte) ---> Otomatik olarak bu işlem sonucu float a promote edilir.
+        //(myDouble * myShort) ---> Otomatik olarak bu işlem sonucu double a promote edilir.
+        double result = (myFloat * myByte) + (myInt / myChar) + (myDouble * myShort);
+
+        System.out.println("result = " + result);
+        int resultInt = (int) result;
+        System.out.println("resultInt = " + resultInt);
+
+        myInt = 25;
+        myFloat = 45.25f;
+        myDouble = 15.75;
+        myLong = 746782678378L;
+
+        float resultFloat = myFloat + myInt; //Otomatik olarak bu işlem sonucu float a promote edilir.
+        long resultLong = myLong + myInt; //Otomatik olarak bu işlem sonucu long a promote edilir.
+        double resultDouble = myDouble + myFloat; //Otomatik olarak bu işlem sonucu double a promote edilir.
+
+        System.out.println("resultFloat = " + resultFloat);
+        System.out.println("resultLong = " + resultLong);
+        System.out.println("resultDouble = " + resultDouble);
+    }
+}
