2a8936442d078e61a5b74bea30250b18f6990c56
Armada <yaskan@gmail.com>
2023-03-20 16:35:39 +0300

    Armada-Some Updates

---
+++ src/Day18_REVIEW_Methods/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day18_REVIEW_Methods;
+
+public class Hello {
+    public static void main(String[] args) {
+
+    }
+}
