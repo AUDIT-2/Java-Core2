bf5e8e794ee9a36b512817bfd36937107a3a9bd8
Armada <yaskan@gmail.com>
2023-04-10 22:21:33 +0300

    Armada-Some Updates

---
+++ src/Day28_REVIEW_Arrays/StringtoCharArray.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day28_REVIEW_Arrays;
+
+import java.util.Arrays;
+
+public class StringtoCharArray {
+    public static void main(String[] args) {
+        String str = "Ankara";
+        char[] harfler = new char[str.length()];
+        harfler = str.toCharArray();
+
+        System.out.println("Arrays.toString(harfler) = " + Arrays.toString(harfler));
+
+        for (int i = 0; i < harfler.length; i++) {
+            System.out.print(harfler[i] + " ");
+        }
+        System.out.println();
+        //Reverse printing
+        for (int i = harfler.length-1; i >= 0; i--) {
+            System.out.print(harfler[i] + " ");
+        }
+    }
+}
