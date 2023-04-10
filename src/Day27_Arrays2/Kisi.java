76552e95941c01e8a670a70c9c42567ad373f6e6
Armada <yaskan@gmail.com>
2023-04-10 21:24:50 +0300

    Armada-Some Updates

--- src/Day27_Arrays2/Kisi.java
+++ src/Day27_Arrays2/Kisi.java
@@ -13,11 +13,14 @@ public class Kisi {
     int telNo;
     String adres;
 
+    public Kisi(){
+
+    }
     public Kisi(String adi,String soyadi, int telNo, String adres){
-        this.adi = adi;
-        this.soyadi = soyadi;
+        this.adi = adi.trim();
+        this.soyadi = soyadi.trim();
         this.telNo = telNo;
-        this.adres = adres;
+        this.adres = adres.trim();
     }
     public void printInfo(){
         System.out.println("Adı           : " + this.adi);
@@ -35,7 +38,7 @@ public class Kisi {
         System.out.println("-------------------------------------");
         int sayac = 0;
         for (Kisi kisi :rehber){
-            if (kisi!=null && kisi.adi != null && kisi.adi.equals(isim)){
+            if (kisi!=null && kisi.adi != null && kisi.adi.contains(isim)){
                 sayac++;
                 kisi.printInfo();
                 System.out.println("------------------");
@@ -48,13 +51,55 @@ public class Kisi {
 
     public static void soyadtanBul(Kisi[] rehber, String soyad){
         //Odev olarak bu metodu tamamlayınız.
+        System.out.println("İsimden bul metodu çalıştı");
+        System.out.println("Rehberimdeki " + soyad + " kişilerin bilgileri");
+        System.out.println("-------------------------------------");
+        int sayac = 0;
+        for (Kisi kisi :rehber){
+            if (kisi!=null && kisi.soyadi != null && kisi.adi.contains(soyad)){
+                sayac++;
+                kisi.printInfo();
+                System.out.println("------------------");
+            }
+        }
+        if(sayac == 0){
+            System.out.println("Rehberde " + soyad + " soyadlı kişiye ait bilgiye rastlanmadı!");
+        } else System.out.println(sayac + " kayıt listelendi.");
     }
 
-    public static void advesoyadtanBul(Kisi[] rehber, String adsoyad){
+    public static void advesoyadtanBul(Kisi[] rehber, String isim, String soyad){
         //Odev olarak bu metodu tamamlayınız.
+        System.out.println("İsimden bul metodu çalıştı");
+        System.out.println("Rehberimdeki " + isim + " " + soyad + " kişilerin bilgileri");
+        System.out.println("-------------------------------------");
+        int sayac = 0;
+        for (Kisi kisi :rehber){
+            if (kisi!=null && kisi.adi!= null && kisi.soyadi!= null && kisi.adi.contains(isim) && kisi.soyadi.contains(soyad)){
+                sayac++;
+                kisi.printInfo();
+                System.out.println("------------------");
+            }
+        }
+        if(sayac == 0){
+            System.out.println("Rehberde " + isim+ " " + soyad + " ad ve soaydlı kişiye ait bilgiye rastlanmadı!");
+        } else System.out.println(sayac + " kayıt listelendi.");
     }
     public static void telefonBul(Kisi[] rehber, int telefon){
         //Odev olarak bu metodu tamamlayınız.
+        System.out.println("İsimden bul metodu çalıştı");
+        System.out.println("Rehberimdeki " + telefon + " telefon numaralı kişilerin bilgileri");
+        System.out.println("-------------------------------------");
+        int sayac = 0;
+        for (Kisi kisi :rehber){
+            if (kisi!=null && kisi.telNo == telefon){
+                sayac++;
+                kisi.printInfo();
+                System.out.println("------------------");
+            }
+        }
+        if(sayac == 0){
+            System.out.println("Rehberde " + telefon + " telefon numaralı kişi kaydına rastlanmadı!");
+        } else System.out.println(sayac + " kayıt listelendi.");
     }
 
     //Klastan nesne üretmeden metodu kullanabilmek için static olarak tanımladık. Klasa ait bir metod olmuş oldu.
--- src/Day27_Arrays2/KisiRehberProg.java
+++ src/Day27_Arrays2/KisiRehberProg.java
@@ -9,32 +9,38 @@ package Day27_Arrays2;
 
 public class KisiRehberProg {
     public static void main(String[] args) {
-        Kisi[] rehberim = new Kisi[4];
+        Kisi[] rehberim = new Kisi[5];
 
-        Kisi kisi1 = new Kisi("Ahmet","Falanca",12345,"Bilinmeyen bir yer.");
-        Kisi kisi2 = new Kisi("Fatma","Gül",34512,"ABD/New York");
-        Kisi kisi3 = new Kisi("Mery","Crismas",17890,"Germany/Berlin");
+        Kisi kisi1 = new Kisi("Ahmet Bekir","Falanca",12345,"Bilinmeyen bir yer.");
+        Kisi kisi2 = new Kisi("Ahat Fatma","Falanca",34512,"ABD/New York");
+        Kisi kisi3 = new Kisi("Mery Ahmet  ","Crismas",17890,"Germany/Berlin");
         Kisi kisi4 = new Kisi("Ahmet","Smith",34567,"Türkiye/Ankara");
+
+        Kisi kisi5 = new Kisi();
 
         rehberim[0] = kisi1;
         rehberim[1] = kisi2;
         rehberim[2] = kisi3;
         rehberim[3] = kisi4;
+        rehberim[4] = kisi5;
+
 
         System.out.println("Rehberimde kayıtlı kişilerin listesi");
-        for (Kisi kisi :rehberim){
-            /*
+       /* for (Kisi kisi :rehberim){
+            *//*
             System.out.println("kisi.adi = " + kisi.adi);
             System.out.println("kisi.soyadi = " + kisi.soyadi);
             System.out.println("kisi.telNo = " + kisi.telNo);
             System.out.println("kisi.adres = " + kisi.adres);
-            */
+            *//*
 
             kisi.printInfo();
         }
-
-        Kisi.isimdenBul(rehberim,"Ahmet");
-        Kisi.listele(rehberim);
+*/
+      //  Kisi.isimdenBul(rehberim,"Ah");
+       //Kisi.advesoyadtanBul(rehberim,"Ah","Fa");
+        Kisi.telefonBul(rehberim,3456722);
+  //    Kisi.listele(rehberim);
 
     }
 }
