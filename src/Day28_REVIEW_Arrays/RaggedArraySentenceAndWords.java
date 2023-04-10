e8b329a3788d490d35cfa25848bb72b7f64414a3
Armada <yaskan@gmail.com>
2023-04-10 21:33:51 +0300

    Armada-Some Updates

---
+++ src/Day28_REVIEW_Arrays/RaggedArraySentenceAndWords.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day28_REVIEW_Arrays;
+
+public class RaggedArraySentenceAndWords {
+    //Ragged arrayleri buradaki örnekte de olduğu gibi bir doküman içerisindeki cümleleri
+    // ve cümleleri oluşturan farklı sayıdaki kelimeleri ayrıştırmak için kullanabiliriz
+
+    String text = "Bu içerisinde cümleler olan örnek bir dokümandır. İçerisinde bir çok cümle ve cümleler içerisinde kelimeler vardır." +
+            " Cümleler birbirlerinden nokta ile ayrılırlar. Kelimeler ise cümle içerisinde boşluk ile birbirlerinden ayrılırlar.";
+
+}
