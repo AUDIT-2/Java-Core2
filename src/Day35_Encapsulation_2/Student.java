c364e94f3dbaffa50f5bbb426d78b63fc9cd5095
Armada <yaskan@gmail.com>
2023-04-26 21:03:21 +0300

    Armada-Some Updates

---
+++ src/Day35_Encapsulation_2/Student.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day35_Encapsulation_2;
+
+public class Student {
+    private static int id = 2000;
+    private String name;
+    private int studentId;
+    private double payment;
+
+    public Student(String name, double payment) {
+        setId();
+        setName(name);
+        setPayment(payment);
+        setStudentId(id);
+    }
+
+    private void setId(){
+        id++;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId='" + studentId + '\'' +
+                ", payment=" + payment +
+                '}';
+    }
+}
---
+++ src/Day35_Encapsulation_2/Teacher.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day35_Encapsulation_2;
+
+public class Teacher {
+    private static int id = 5000;
+    private int teacherId;
+    private String name;
+    private double salary;
+
+    public Teacher(String name, double salary) {
+        setId();
+        setTeacherId(id);
+        setName(name);
+        setSalary(salary);
+    }
+
+    private void setId() {
+        id++;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int id) {
+        this.teacherId = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherId=" + teacherId +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
