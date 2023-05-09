b569d18c3a2d9d8bd2a02b4b9fb84e10eabe90c5
Armada <yaskan@gmail.com>
2023-05-09 23:00:08 +0300

    Armada-Some Updates

---
+++ src/Day41_Final/FinalArray.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day41_Final;
+
+import java.util.Arrays;
+
+public class FinalArray {
+    int[] arrNumber = new int[3];
+    final int[] fnlArrayNumber = new int[3];
+
+    public static void main(String[] args) {
+        FinalArray obj = new FinalArray();
+        obj.arrNumber[0] = 178;
+        obj.arrNumber[1] = 3;
+        obj.arrNumber[2] = 90;
+
+        System.out.println(Arrays.toString(obj.arrNumber));
+
+        System.out.println();
+        System.out.println("For...");
+        for (int i = 0; i < obj.arrNumber.length; i++) {
+            System.out.println(obj.arrNumber[i]);
+        }
+
+        obj.arrNumber[1] = 678;
+        System.out.println();
+        System.out.println("For each...");
+        for (int eleman: obj.arrNumber){
+            System.out.println(eleman);
+        }
+
+        System.out.println("FINAL ARRAY.....");
+        obj.fnlArrayNumber[0] = 34;
+        obj.fnlArrayNumber[1] = 123;
+        obj.fnlArrayNumber[2] = 987;
+
+        System.out.println(Arrays.toString(obj.fnlArrayNumber));
+        obj.fnlArrayNumber[1] = 33456;
+
+        System.out.println(Arrays.toString(obj.fnlArrayNumber));
+
+        int[] birArray = new int[3];
+
+        System.out.println(Arrays.toString(obj.arrNumber));
+        obj.arrNumber = birArray;
+        System.out.println(Arrays.toString(obj.arrNumber));
+
+        System.out.println("FINAL ARRAY");
+       // obj.fnlArrayNumber = birArray; Error- fnlArrayNumber declared as final, can not be assigned another arrays reference
+
+    }
+}
---
+++ src/Day41_Final/FinalArrayList.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023.
+ * SDET- 2 Java Core
+ * Armada-prog
+ * armada.dscrd@gmail.com
+ */
+
+package Day41_Final;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FinalArrayList {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    final ArrayList<Integer> fnlArrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        FinalArrayList obj = new FinalArrayList();
+
+        //Normal Array list
+        obj.arrayList.add(123);
+        obj.arrayList.add(345);
+        obj.arrayList.add(5);
+        obj.arrayList.add(2345);
+        System.out.println("ArrayList :" + Arrays.asList(obj.arrayList));
+
+        //Final Array list
+        obj.fnlArrayList.add(667);
+        obj.fnlArrayList.add(67);
+        obj.fnlArrayList.add(745);
+        obj.fnlArrayList.add(9867);
+
+        System.out.println("Final ArrayList :" + Arrays.asList(obj.fnlArrayList));
+        obj.fnlArrayList.remove(1);
+        System.out.println();
+        System.out.println("Final ArrayList :" + Arrays.asList(obj.fnlArrayList));
+
+        obj.arrayList = obj.fnlArrayList;
+        System.out.println("ArrayList :" + Arrays.asList(obj.arrayList));
+
+       // obj.fnlArrayList = obj.arrayList; Error
+    }
+}
