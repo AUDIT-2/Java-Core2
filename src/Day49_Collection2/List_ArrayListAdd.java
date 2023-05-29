feb4dd1c6331fd81864b9f51b137932149850af5
Armada <yaskan@gmail.com>
2023-05-29 21:06:12 +0300

    Armada-Some Updates

--- src/Day49_Collection2/List_ArrayListAdd.java
+++ src/Day49_Collection2/List_ArrayListAdd.java
@@ -7,6 +7,30 @@
 
 package Day49_Collection2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class List_ArrayListAdd {
+    public static void main(String[] args) {
+        System.out.println("Inserting an element to an ArrayList.");
+        List<String> colors = new ArrayList<>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Orange");
+        colors.add("White");
+        colors.add("Black");
+
+        System.out.println("Original list : " + colors);
+        colors.add(0,"Pink");
+        System.out.println("Updated list  : " + colors);
+
+        //Traversing ArrayList by with using  for index
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println("Element index : " + i  + "-" + colors.get(i));
+        }
+
+
+    }
 
 }
