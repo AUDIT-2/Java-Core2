2eee6b85747f897f2b4e1c96f71408663ec816fc
Armada <yaskan@gmail.com>
2023-05-31 01:35:08 +0300

    Armada-Some Updates

--- src/Day50_Collection3/MarketQueue/Kasiyer.java
+++ src/Day50_Collection3/MarketQueue/Kasiyer.java
@@ -22,23 +22,27 @@ public class Kasiyer extends Thread {
         this.musteriler = musteriler;
         this.islemYapilanMusteriler = new ArrayList<>();
     }
-
     //.run() metodu herbir kasiyer için ayrı thread de çalışır. Multi threaded çalışacak kodlar bu metod içerisinde yazılmalıdır.
     @Override
     public void run() {
         while (!musteriler.isEmpty()){
             int sure = 1000 + (int) (Math.random() * 9000);//Rastgele bir süre oluşturur.(1-10 saniye)
             String musteri = musteriler.poll(); //Bu kasiyer bekleyen müşterilerden sıradakini çağırır
-            islemYapilanMusteriler.add(musteri); //Bu kasiyer işlem yaptığı müşteriyi kendi listesine ekler
-            toplamCalismaSure += sure; //Bu kasiyerin toplam çalışma süresi
-            System.out.println("Kasiyer - " + kasiyerNo + " müşteri - " + musteri + " süre : " + sure);
-            try {
-                Thread.sleep(sure); //Rastgele belirlediğimiz süre bu thread ı bekletiyoruz.
-            }catch (InterruptedException e){
-                System.out.println("Hata oluştu....");
+            if(musteri != null){
+                System.out.println("Müşteri : " + musteri + " lütfen kasiyer -> " + kasiyerNo);
+                islemYapilanMusteriler.add(musteri); //Bu kasiyer işlem yaptığı müşteriyi kendi listesine ekler
+                toplamCalismaSure += sure; //Bu kasiyerin toplam çalışma süresi
+                System.out.println("Kasiyer -" + kasiyerNo + " müşteri -" + musteri + " işlem süresi : " + sure);
+                try {
+                    Thread.sleep(sure); //Rastgele belirlediğimiz süre bu thread ı bekletiyoruz.
+                }catch (InterruptedException e){
+                    System.out.println("Hata oluştu....");
+                }
             }
         }
-        System.out.println("İşlem yapan kasiyer : " + kasiyerNo + "---> Müşteriler : " +
-                islemYapilanMusteriler + " -Toplam çalışma süresi : " + toplamCalismaSure);
+        if(!islemYapilanMusteriler.isEmpty()){
+            System.out.println("İşlem yapan kasiyer : " + kasiyerNo + "---> Müşteriler : " +
+                    islemYapilanMusteriler + " -Toplam çalışma süresi : " + toplamCalismaSure);
+        }
     }
 }
